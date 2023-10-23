b34856508cf9cac5e6624a55d735b019e8df06d0
Icram Doku <icramdoku@gmail.com>
2023-10-22 20:43:43 -0400

    use bcryptjs

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs"); // Use bcryptjs instead of bcrypt
 const jwt = require("jsonwebtoken");
 const User = require("../../models/userModel");
 const crypto = require("crypto");
