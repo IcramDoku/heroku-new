181edadba0c757703fe202a41cb1630cd065dcc1
IcramDoku <vt69093@umbc.edu>
2023-11-18 01:31:32 -0500

    updated code as of nov 18th

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 })
 
 // Schedule the cron job to run every day at 8:00 AM in a specific time zone :'America/New_York'
-cron.schedule('31 1 * * *', async () => {
+cron.schedule('33 1 * * *', async () => {
   try {
     console.log('Cron job triggered at:', new Date().toLocaleString());
     await sendTaskNotifications();
