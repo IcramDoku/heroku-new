7f8071bb6cfe78379d4043c904cf8ca6116d46a5
IcramDoku <107223903+IcramDoku@users.noreply.github.com>
2023-12-04 17:09:24 -0500

    Update AllTasksModel.js

--- models/AllTasksModel.js
+++ models/AllTasksModel.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const AllTasksSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  groupId: { type: mongoose.Schema.Types.ObjectId, ref: "Group" },
-  taskId: { type: mongoose.Schema.Types.ObjectId, ref: "Task" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+  groupId: { type: mongoose.Schema.Types.ObjectId, ref: "Group", required: true },
+  taskId: { type: mongoose.Schema.Types.ObjectId, ref: "Task", required: true },
 });
 
 const AllTasks = mongoose.model("AllTasks", AllTasksSchema);
